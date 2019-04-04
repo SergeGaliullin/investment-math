d427c7d384429ec475bc62758876d00d3730e360
serge <sergei.galiullin@gmail.com>
2019-04-04 19:05:28 -0300

    Add future/present value and tests

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,31 @@
+
 fn main() {
-    println!("Hello, world!");
+    println!("Future value: {}", future_value(42000.0, 1.05, 2));
+    println!("Present value: {}", present_value(50000.0, 1.05, 2));
 }
+
+
+fn future_value(investment: f64, interest_rate: f64, time_unit: u16) -> f64 {
+    investment * interest_rate.powi(time_unit as i32)
+}
+
+fn present_value(future_value: f64, interest_rate: f64, time_unit: u16) -> f64 {
+    future_value / interest_rate.powi(time_unit as i32)
+}
+
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_future_value() {
+        assert_eq!(46305.0, future_value(42000.0, 1.05, 2u16))
+    }
+
+    #[test]
+    fn test_present_value() {
+        assert_eq!(45351.47392290249, present_value(50000.0, 1.05, 2u16))
+    }
+}
+
